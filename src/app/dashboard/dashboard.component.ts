7e7f69cd39bb4b8a2d60409c6534cdf0ab019fca
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-02-18 01:45:30 -0800

    adding dynamic nature for Dashboard using ngFor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,7 +24,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.Designation = 'Team Leader';
-    this.Username = 'Scott Smith';
+    this.Username = 'Jessica Smith';
     this.NoOfTeamMembers = 67;
     this.TotalCostOfAllProjects = 240;
     this.PendingTasks = 15;
@@ -32,5 +32,57 @@ export class DashboardComponent implements OnInit {
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+    this.Clients = ['ABC Infotech Ltd.', 'DEF Software Solutions', 'GHI Industries'];
+    this.Projects = ['Project A', 'Project B', 'Project C', 'Project D'];
+
+    for (let i = 2021; i >= 2015; i--) {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      { Region: 'East', TeamMembersCount: 20, TemporarilyUnavailableMembers: 4},
+      { Region: 'West', TeamMembersCount: 15, TemporarilyUnavailableMembers: 8},
+      { Region: 'South', TeamMembersCount: 17, TemporarilyUnavailableMembers: 2},
+      { Region: 'North', TeamMembersCount: 15, TemporarilyUnavailableMembers: 6}
+    ];
+    
+    this.TeamMembers = [
+      {
+        Region: 'East',
+        Members: [
+          { ID: 1, Name: 'Ford', Status: 'Available' },
+          { ID: 2, Name: 'Miller', Status: 'Available' },
+          { ID: 3, Name: 'Jones', Status: 'Busy' },
+          { ID: 4, Name: 'James', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'West',
+        Members: [
+          { ID: 5, Name: 'Anna', Status: 'Available' },
+          { ID: 6, Name: 'Arun', Status: 'Available' },
+          { ID: 7, Name: 'Varun', Status: 'Busy' },
+          { ID: 8, Name: 'Jasmine', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'South',
+        Members: [
+          { ID: 9, Name: 'Krishna', Status: 'Available' },
+          { ID: 10, Name: 'Mohan', Status: 'Available' },
+          { ID: 11, Name: 'Raju', Status: 'Busy' },
+          { ID: 12, Name: 'Farooq', Status: 'Busy' },
+        ],
+      },
+      {
+        Region: 'North',
+        Members: [
+          { ID: 13, Name: 'Jacob', Status: 'Available' },
+          { ID: 14, Name: 'Smith', Status: 'Available' },
+          { ID: 15, Name: 'Jones', Status: 'Busy' },
+          { ID: 16, Name: 'James', Status: 'Busy' },
+        ],
+      },
+    ];
   }
 }
