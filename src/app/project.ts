870f273c2f10136709273c05844fb464cb38f906
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-03-28 12:12:03 -0700

    added project model and services for AJAX request

---
+++ src/app/project.ts
@@ -0,0 +1,13 @@
+export class Project {
+    projectID: number;
+    projectName: string;
+    dateOfStart: string;
+    teamSize: string;
+
+    constructor() {
+        this.projectID = 0;
+        this.projectName = null;
+        this.dateOfStart = null;
+        this.teamSize = null;
+    }
+}
---
+++ src/app/projects.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Project } from './project';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  //since it is private the ref will be accessed in all the methods of the current service
+  constructor(private httpClient : HttpClient) { } //here httpClient is ref to access obj of HttpClient service
+
+  //ready to return an array of Project that is received from server as response
+  getAllProjects() : Observable<Project[]>
+  {
+    return this.httpClient.get<Project[]>("/api/projects");
+  }
+}
